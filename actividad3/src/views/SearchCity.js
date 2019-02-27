b6bd95c161d43252a20c8b25d77256660c9b6a4c
Angel Virgilio Cueto González <AngelCueto@MacBook-Pro-de-Samuel-2.local>
2019-02-27 17:55:29 -0600

    Routing, table and row componets

--- actividad3/src/views/SearchCity.js
+++ actividad3/src/views/SearchCity.js
@@ -1,21 +1,44 @@
 import React, { Component } from 'react';
 //import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Icon from '@material-ui/core/Icon';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Typography from '@material-ui/core/Typography';
 
-
+import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
 
 import './SearchCity.scss';
 
 
 class SearchCity extends Component {
 
-   
+
 
 
 
     render() {
         return (
 
-            <p> Search City Component</p>
+            <div>
+                <Typography variant="h6" color="inherit" >
+                    Consult Weather from a City
+                </Typography>
+                <TextField  label="City"  margin="normal" InputProps={{
+                        endAdornment: <InputAdornment position="end"><Icon>AccessAlarmIcon</Icon></InputAdornment>,
+                    }} variant="filled" /> 
+                <div ></div>
+                <Button variant="outlined" >
+                    Consult
+                </Button>
+
+
+
+
+            </div>
+
+
+          
 
         );
     }
