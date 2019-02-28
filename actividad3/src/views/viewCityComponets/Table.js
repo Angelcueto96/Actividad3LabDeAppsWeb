e9e60f22b5aae612f2a8e3095f5bfd563b55ef3f
anasollano <anasollanokim@gmail.com>
2019-02-27 19:14:13 -0600

    imports

--- actividad3/src/views/viewCityComponets/Table.js
+++ actividad3/src/views/viewCityComponets/Table.js
@@ -1,17 +1,10 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
-
-
+var CityWeatherStore = require('../stores/CityWeatherStore');
+var CityWeatherActions = require('../actions/CityWeatherActions');
 //import './Vie.scss';
 
-
 class Table extends Component {
-
-   
-
-
-
     render() {
         return (
 
